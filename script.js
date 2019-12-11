10831bb468fef33c970f7e5aac1e0af3abfbf230
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2019-12-11 16:28:45 +0100

    typed.js added and styling on navbar elements

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-import Typed from 'typed.js';
+var options = {
+  strings: ['Hi I\'m Jon.', 'It\'s a pleasure meeting you.', 'This website is a WIP'],
+  typeSpeed: 50,
+  loop: true
+};
 
-const loadDynamicBannerText = () => {
-  new Typed('#banner-typed-text', {
-    strings: ["Change your life", "Learn to code"],
-    typeSpeed: 50,
-    loop: true
+var typed = new Typed('.element', options);
+
+var navbar = document.querySelector('.navigation');
+if (navbar) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY >= window.innerHeight) {
+      navbar.classList.add('navigation-white');
+    } else {
+      navbar.classList.remove('navigation-white');
+    }
   });
 }
-
-loadDynamicBannerText();
