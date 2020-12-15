0e3dddf44052a502fcce05c45f46a373513cbf27
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2020-12-15 16:41:03 +0100

    links and page styling

--- script.js
+++ script.js
@@ -7,12 +7,28 @@ var options = {
 var typed = new Typed('.element', options);
 
 var navbar = document.querySelector('.navigation');
+var navigationLinks = document.querySelectorAll('.navigation a');
+
+// navigationLinks.forEach(link => {
+//   link.addEventListener('hover', () => {
+//     console.log("helo");
+//   });
+// })
+
 if (navbar) {
   window.addEventListener('scroll', () => {
     if (window.scrollY >= window.innerHeight) {
       navbar.classList.add('navigation-white');
+      navigationLinks.forEach(link => {
+        link.style.color = "black";
+        link.style.boxColor = "  box-shadow: 1px 2px 5px rgba(80, 80, 80, 0.9);";
+      });
     } else {
       navbar.classList.remove('navigation-white');
+      navigationLinks.forEach(link => {
+        link.style.backgroundColor = "transparent";
+        link.style.color = "white";
+      })
     }
   });
 }
